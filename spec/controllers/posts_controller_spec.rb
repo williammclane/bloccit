cff711b40c5f0bed5789d4fbd599ad7533f9536f
William McLane <dukeoftinmen@gmail.com>
2017-08-30 01:45:45 +0000

    checkpoint18 done

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,17 +4,25 @@ RSpec.describe PostsController, type: :controller do
   
   let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    describe "GET show" do
+     it "returns http success" do
+ # #16
+       get :show, params: { id: my_post.id }
+       expect(response).to have_http_status(:success)
+     end
+     it "renders the #show view" do
+ # #17
+       get :show, params: { id: my_post.id }
+       expect(response).to render_template :show
+     end
+ 
+     it "assigns my_post to @post" do
+       get :show, params: { id: my_post.id }
+ # #18
+       expect(assigns(:post)).to eq(my_post)
+     end
     end
-    it "assigns [my_post] to @posts" do
-       get :index
- # #9
-       expect(assigns(:posts)).to eq([my_post])
-    end 
-  end
+    
     describe "GET new" do
       it "returns http success" do
         get :new
@@ -52,13 +60,6 @@ RSpec.describe PostsController, type: :controller do
         expect(response).to redirect_to Post.last
       end
     end
-
- # describe "GET new" do
- #   it "returns http success" do
- #     get :new
- #     expect(response).to have_http_status(:success)
- #   end
- # end
 
  #  describe "GET edit" do
  #    it "returns http success" do
