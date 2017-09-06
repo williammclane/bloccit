3a48b8a2d573bb9493f0299adf671edb47f8a6c5
William McLane <dukeoftinmen@gmail.com>
2017-09-06 05:18:47 +0000

    assignment24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
    def new
      @user = User.new
    end
+   def confirm
+     @user = User.new
+     @user.name = params[:user][:name]
+     @user.email = params[:user][:email]
+     @user.password = params[:user][:password]
+     @user.password_confirmation = params[:user][:password_confirmation]
+   end
    def create
  # #9
      @user = User.new
