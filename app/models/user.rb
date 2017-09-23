6a710b314c9d159e9d148911dc84c7ecd1e81cf2
William McLane <dukeoftinmen@gmail.com>
2017-09-23 00:08:37 +0000

    checkpoint29

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
    has_many :posts, dependent: :destroy
    has_many :comments, dependent: :destroy
+   has_many :votes, dependent: :destroy
    before_save { self.email = email.downcase if email.present? }
    before_save { self.role ||= :member }
  # #3
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
  # #4
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-  validates :password, length: { minimum: 6 }, allow_blank: true
+   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+   validates :password, length: { minimum: 6 }, allow_blank: true
  # #5
    validates :email,
              presence: true,
